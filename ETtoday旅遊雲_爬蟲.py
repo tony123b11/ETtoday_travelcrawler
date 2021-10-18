76fb709126e4402ecb1554b607f3dbe24f75937d
TonyChang <tony123b11@gmail.com>
2021-10-18 15:30:39 +0800

    增加輸入除錯(一定要是數字)

--- ETtoday旅遊雲_爬蟲.py
+++ ETtoday旅遊雲_爬蟲.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-search_herf = "https://travel.ettoday.net/category/%E5%8F%B0%E4%B8%AD/?&page="
+
 page = input('請輸入瀏覽頁數：')
-page_count = int(page)
 
 
 def web_html(page):
     response = requests.get(page)
     soup = BeautifulSoup(response.text, "html.parser")
     get_title(soup)
+    return(page)
 
 
 def get_title(soup):
@@ -18,8 +18,15 @@ def get_title(soup):
         print(title.select_one("a").get("href"))
 
 
-for pages in range(1, page_count+1):
-    print()
-    print("ETtoday旅遊雲,第"+str(pages)+"頁")
-    search_herf = search_herf+str(pages)
-    test = web_html(search_herf)
+try:
+    page_count = int(page)
+    for pages in range(1, page_count+1):
+        print()
+        # print("ETtoday旅遊雲,第"+str(pages)+"頁")
+        search_herf = "https://travel.ettoday.net/category/%E5%8F%B0%E4%B8%AD/?&page="
+        search_herf = search_herf+str(pages)
+        test = web_html(search_herf)
+        print("ETtoday旅遊雲,第"+str(pages)+"頁網址:"+test)
+
+except:
+    print("請輸入數字!")
