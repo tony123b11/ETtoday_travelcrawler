70a73bf255c1fdcbc4f024b87af67c9fa499d220
TonyChang <tony123b11@gmail.com>
2021-10-20 14:22:43 +0800

    基本除錯 1.輸入只能是數字，若不是會出現錯誤提示，並再讓使用者在輸入一次 2.顯示頁面不會將沒有的頁面輸出

--- ETtoday旅遊雲_爬蟲.py
+++ ETtoday旅遊雲_爬蟲.py
@@ -1,32 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = input('請輸入瀏覽頁數：')
+
+def get_user_input():
+    page = input('請輸入瀏覽頁數：')
+    try:
+        page_int = int(page)
+       # print(type(page_int))
+        return(page_int)
+    except Exception as e:
+        print(e)
+        print("請輸入數字!")
+        page_int = False
+        return(page_int)
 
 
-def web_html(page):
+def print_result(page_result):
+    for pages in range(1, page_result+1):
+        # print("ETtoday旅遊雲,第"+str(pages)+"頁")
+        search_herf = "https://travel.ettoday.net/category/%E5%8F%B0%E4%B8%AD/?&page="
+        search_herf = search_herf+str(pages)
+        test = web_html(search_herf, pages)
+
+
+def web_html(page, test):
     response = requests.get(page)
     soup = BeautifulSoup(response.text, "html.parser")
-    get_title(soup)
+    exsist = get_title(soup)
+    if exsist:
+        # print("null")
+        print("ETtoday旅遊雲,第"+str(test)+"頁網址:"+page)
+        print()
+    else:
+        print(soup)
     return(page)
 
 
 def get_title(soup):
     result = soup.find_all("div", itemprop="itemListElement")
+
     for title in result:
         print(title.select_one("a").get("title"))
         print(title.select_one("a").get("href"))
+    if not result:
+        exsist_bool = False
+        return(exsist_bool)
+    else:
+        exsist_bool = True
+        return(exsist_bool)
 
 
-try:
-    page_count = int(page)
-    for pages in range(1, page_count+1):
-        print()
-        # print("ETtoday旅遊雲,第"+str(pages)+"頁")
-        search_herf = "https://travel.ettoday.net/category/%E5%8F%B0%E4%B8%AD/?&page="
-        search_herf = search_herf+str(pages)
-        test = web_html(search_herf)
-        print("ETtoday旅遊雲,第"+str(pages)+"頁網址:"+test)
+# 3
+page = get_user_input()
+bool = True
+while bool:
+    if page:
+        # print(page)
+        print_result(page)
+        bool = False
+    else:
+        page = get_user_input()
+        # print(type(page))
 
-except:
-    print("請輸入數字!")
+# print(type(page))
